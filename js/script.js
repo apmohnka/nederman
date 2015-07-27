38a4050ad23072800650d7cf89991578c80dd4cc
apmohnka <apmohnka@gmail.com>
2015-07-27 14:01:43 +0200

    spare parts / cart / login

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
-	$('#spinEdit').spinedit({
-	    minimum: 1,
-	    maximum: 10,
-	    step: 1,
-	    value: 0,
-	    numberOfDecimals: 0
-	});
+
+	if($('.spinEdit').spinedit){
+		$('.spinEdit').spinedit({
+		    minimum: 1,
+		    maximum: 10,
+		    step: 1,
+		    value: 0,
+		    numberOfDecimals: 0
+		});
+	}
+
+	$('.combobox').combobox();
+	$('.date-picker').datepicker();
 });
